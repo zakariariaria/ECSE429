a05ae8d959f032f8302688035eb4ee03726183fc
zakariariaria <zakaria.bouhemhem@mail.mcgill.ca>
2024-02-01 15:19:31 -0500

    added application .jar

--- PartA/src/test/java/ca/mcgill/ecse429/api_testing/TodoAPITest.java
+++ PartA/src/test/java/ca/mcgill/ecse429/api_testing/TodoAPITest.java
@@ -6,15 +6,25 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 
 @TestMethodOrder(MethodOrderer.Random.class)
 
 public class TodoAPITest {
     
-    @Test
-    public void test1() {
-    	int test = 5;
-    	assertEquals(5,test);
+	@Test
+    public void testGetTodos() {
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/todos").
+        then().
+            assertThat().
+            statusCode(200);
     }
     @Test
     public void test2() {
