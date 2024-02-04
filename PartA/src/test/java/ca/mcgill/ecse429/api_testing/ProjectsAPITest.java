ef8c42964cdf56ad901af2a3682566b75577cbe6
menadkessaci <menad.kessaci@mail.mcgill.ca>
2024-02-04 00:06:33 -0500

    All basic tests set, just need to write them

--- PartA/src/test/java/ca/mcgill/ecse429/api_testing/ProjectsAPITest.java
+++ PartA/src/test/java/ca/mcgill/ecse429/api_testing/ProjectsAPITest.java
@@ -15,7 +15,8 @@ import static org.hamcrest.Matchers.*;
 
 public class ProjectsAPITest {
 
-    
+    //"Project/:id"
+    //Get
    @Test
     public void testGetAllProjects() {
 
@@ -29,6 +30,7 @@ public class ProjectsAPITest {
             statusCode(200);
     }
 
+    //Put
     @Test
     public void testPutNotAllowed() {
 
@@ -42,6 +44,7 @@ public class ProjectsAPITest {
             statusCode(200);
     }
 
+    //Post
     @Test
     public void testPost201() {
 
@@ -55,6 +58,7 @@ public class ProjectsAPITest {
             statusCode(200);
     }
 
+    //Post
     @Test
     public void testPost400() {
 
@@ -68,6 +72,7 @@ public class ProjectsAPITest {
             statusCode(200);
     }
 
+    //Delete
     @Test
     public void testDeleteNotAllowed() {
 
@@ -95,6 +100,7 @@ public class ProjectsAPITest {
             statusCode(200);
     }
 
+    //Head
     @Test
     public void testReturnHeaders200() {
 
@@ -108,6 +114,7 @@ public class ProjectsAPITest {
             statusCode(200);
     }
 
+    //Patch
     @Test
     public void testPatchNotAllowed405() {
 
@@ -121,59 +128,309 @@ public class ProjectsAPITest {
             statusCode(200);
     }
 
-    //"Project/:id"
 
+    //"Project/:id"
     //Get
+    @Test
+    public void testSample1() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Put
+    @Test
+    public void testSample2() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Post
+    @Test
+    public void testSample3() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Delete
+    @Test
+    public void testSample4() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Options
+    @Test
+    public void testSample5() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Head
+    @Test
+    public void testSample6() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Patch
+    @Test
+    public void testSample7() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
 
 
 
     //"Project/:id/categories"
-
     //Get
+    @Test
+    public void testSample8() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Put
+    @Test
+    public void testSample9() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Post
+    @Test
+    public void testSampleq() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Delete
+    @Test
+    public void testSamplew() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Options
+    @Test
+    public void testSamplee() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Head
+    @Test
+    public void testSampler() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Patch
+    @Test
+    public void testSamplet() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
 
 
 
     //"/projects/:id/categories/:id"
-
     //Get
+    @Test
+    public void testSampley() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Put
+    @Test
+    public void testSampleu() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Post
+    @Test
+    public void testSamplei() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Delete
+    @Test
+    public void testSampleo() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Options
+    @Test
+    public void testSamplep() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Head
+    @Test
+    public void testSamplea() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Patch
+    @Test
+    public void testSamples() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
 
 
@@ -181,20 +438,103 @@ public class ProjectsAPITest {
 
 
     //"/projects/:id/tasks"
-
     //Get
+    @Test
+    public void testSampled() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Put
+    @Test
+    public void testSamplef() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Post
+    @Test
+    public void testSampleg() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Delete
+    @Test
+    public void testSampleh() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Options
+    @Test
+    public void testSamplej() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Head
+    @Test
+    public void testSamplek() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Patch
+    @Test
+    public void testSamplel() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
 
 
@@ -203,18 +543,101 @@ public class ProjectsAPITest {
 
 
     //"/projects/:id/tasks/:id"
-
     //Get
+    @Test
+    public void testSamplez() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Put
+    @Test
+    public void testSamplex() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Post
+    @Test
+    public void testSamplec() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Delete
+    @Test
+    public void testSamplev() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Options
+    @Test
+    public void testSampleb() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Head
+    @Test
+    public void testSamplen() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 
     //Patch
+    @Test
+    public void testSamplem() {
+
+        RestAssured.baseURI = "http://localhost:4567";
+
+        given().
+        when().
+            get("/projects").
+        then().
+            assertThat().
+            statusCode(200);
+    }
 }
