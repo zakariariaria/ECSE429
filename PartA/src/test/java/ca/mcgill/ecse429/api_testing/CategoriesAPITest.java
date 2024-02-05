ac9d0f28597bcd2dac5e43b6b2df6002f1bd5387
Yida Pan <68798199+frankie453@users.noreply.github.com>
2024-02-05 13:37:42 -0500

    Fix dependency issues

---
+++ PartA/src/test/java/ca/mcgill/ecse429/api_testing/CategoriesAPITest.java
@@ -0,0 +1,31 @@
+package ca.mcgill.ecse429.api_testing;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class CategoriesAPITest {
+
+	private static boolean available;
+
+    @BeforeAll
+    public static void checkApiAvailability() {
+        try {
+            int statusCode = given()
+                                .when()
+                                .get("http://localhost:4567/")
+                                .then()
+                                .extract()
+                                .statusCode();
+
+            available = (statusCode == 200);
+        } catch (Exception e) {
+            fail(e.getMessage());
+            available = false;
+        }
+    }
+    
+
+}
